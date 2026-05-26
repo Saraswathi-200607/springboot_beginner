bfb3c81c86c6ffdfdedd176d85881cd7879d62dd
Neelakantam <saraswathigovardhan7@gmail.com>
2026-05-26 08:20:38 +0530

    js commit

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,18 +1,15 @@
-console.log("Hello from JS");
-
-var users = [
-    {"name" : "John", "gender" : "Male", "img" : "img/img_avatar.png"},
-    {"name" : "Jane", "gender" : "Female", "img" : "img/img_avatar2.png"}
+const users = [
+{ name:"John Doe",   gender:"Male",
+image:"/img/img_avatar.png" },
+{ name:"Jane Doe",   gender:"Female",
+image:"/img/img_avatar2.png" }
 ];
-
-var currentUserIndex = 0;
-
-function toggleCard(){
-    currentUserIndex =(currentUserIndex + 1) % 2;
-    var user = users[currentUserIndex];
-
-    // DOM Manipulation
-    document.getElementById("userImage").src = user.img;
-    document.getElementById("userGender").innerHTML = user.gender;
-    document.getElementById("userName").innerHTML = user.name;
+let current = 0;
+function toggle() {
+current = (current + 1) % users.length;
+const u = users[current];
+document.getElementById("userName")
+.textContent = u.name;
+document.getElementById("userGender")
+.textContent = u.gender;
 }
