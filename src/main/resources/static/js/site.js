0a892217dea510ad4a3b806cbf9995139912714d
Neelakantam <saraswathigovardhan7@gmail.com>
2026-05-26 08:27:58 +0530

    final day1 commit

--- src/main/resources/static/js/site.js
+++ src/main/resources/static/js/site.js
@@ -1,15 +1,26 @@
 const users = [
-{ name:"John Doe",   gender:"Male",
-image:"/img/img_avatar.png" },
-{ name:"Jane Doe",   gender:"Female",
-image:"/img/img_avatar2.png" }
+{
+    name: "John Doe",
+    gender: "Male",
+    image: "/img/img_avatar.png"
+},
+{
+    name: "Jane Doe",
+    gender: "Female",
+    image: "/img/img_avatar2.png"
+}
 ];
+
 let current = 0;
+
 function toggle() {
-current = (current + 1) % users.length;
-const u = users[current];
-document.getElementById("userName")
-.textContent = u.name;
-document.getElementById("userGender")
-.textContent = u.gender;
+    current = (current + 1) % users.length;
+
+    const u = users[current];
+
+    document.getElementById("userName").textContent = u.name;
+
+    document.getElementById("userGender").textContent = u.gender;
+
+    document.getElementById("userImage").src = u.image;
 }
